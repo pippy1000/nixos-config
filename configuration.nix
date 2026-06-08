29daf6b6fe61b172d5ce334710bf082c07b7bc45
pippy1000 <rotmgpippy1000@gmail.com>
2026-06-08 00:41:00 -0500

    restore full configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,56 +1,47 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, hyprland, ... }:
 {
-    imports = [
-    ];
-
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     nixpkgs.config.allowUnfree = true;
-
     networking.hostName = "hyprland-btw";
     networking.networkmanager.enable = true;
-
     time.timeZone = "America/Chicago";
-
     services.getty.autologinUser = "ethan";
     services.upower.enable = true;
     services.blueman.enable = true;
-    
+    services.openssh.enable = true;
+    services.asusd.enable = true;
     hardware.bluetooth.enable = true;
     hardware.bluetooth.powerOnBoot = true;
-
     systemd.services."getty@tty1" = {
         enable = true;
         wantedBy = [ "multi-user.target" ];
     };
-
     programs.hyprland = {
         enable = true;
+        package = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        portalPackage = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
         xwayland.enable = true;
         withUWSM = true;
     };
-
     users.users.ethan = {
         isNormalUser = true;
-        extraGroups = [ "wheel" ];
+        extraGroups = [ "wheel" "networkmanager" "gamemode" ];
+        hashedPassword = "$6$kvrvq7Da3uaaImZn$8cWprF20AI8mp90F4Dry4KVvOMSUZ2kRgWhNRkEY11iolOGaNjLYA/XnkIhA53fWU8HBNltbRa6rmNoW7qORD1";
         packages = with pkgs; [
             tree
         ];
     };
-    
+    users.users.root.hashedPassword = "$6$kvrvq7Da3uaaImZn$8cWprF20AI8mp90F4Dry4KVvOMSUZ2kRgWhNRkEY11iolOGaNjLYA/XnkIhA53fWU8HBNltbRa6rmNoW7qORD1";
     xdg.portal = {
-      enable = true;
-      extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
-    
+        enable = true;
+        extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     };
-
     programs.firefox.enable = true;
-    
     fonts.packages = with pkgs; [
         nerd-fonts.jetbrains-mono
         nerd-fonts.noto
     ];
-
     environment.systemPackages = with pkgs; [
         vim
         wget
@@ -65,13 +56,26 @@
         hyprpanel
         awww
         hyprlock
-        hyprpanel
         fastfetch
         spotify
+        libreoffice
+        neovim
+        nodejs
+        ripgrep
+        gnumake
+        gcc
+        lua-language-server
+        vscode-langservers-extracted
+        intelephense
+        typescript-language-server
+        typescript
+        nil
+        clang-tools
+        alejandra
+        brightnessctl
+        tree-sitter
     ];
-
+    environment.variables.EDITOR = "nvim";
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
-   
     system.stateVersion = "26.05";
-  swapDevices = [{ device = "/var/lib/swapfile"; size = 4096; }];
 }
